d0db9074a8436a40b2dce23c80f8421e0ce57a61
ShadesByte <225610230+ShadesByte@users.noreply.github.com>
2025-12-30 23:36:22 -0600

    Add content to item families

--- app/data/game/constants/itemFamilies.js
+++ app/data/game/constants/itemFamilies.js
@@ -1,5 +1,27 @@
 export const ITEM_FAMILIES_VERSION = '3.0'
 
 export const ITEM_FAMILIES = Object.freeze({
-    
+
+    // Enemy Drop Weapon Skill Materials
+    EXOSWARM_CORE: 'exoswarm_core',
+    EXOSWARM_PENDANT: 'exoswarm_pendant',
+    HOWLER_CORE: 'howler_core',
+    MASK: 'mask',
+    MECH_CORE: 'mech_core',
+    POLYGON_CORE: 'polygon_core',
+    RING: 'ring',
+    TIDAL_RESIDUUM: 'tidal_residuum',
+    WHISPERIN_CORE: 'whisperin_core',
+
+    // Forgery Weapon Skill Materials
+    CADENCE: 'cadence',
+    CARVED_CRYSTAL: 'carved_crystal',
+    COMBUSTOR: 'combustor',
+    HELIX: 'helix',
+    METALLIC_DRIP: 'metallic_drip',
+    PHLOGISTON: 'phlogiston',
+    POLARIZER: 'polarizer',
+    STRING: 'string',
+    WAVEWORN_RESIDUE: 'waveworn_residue',
+    WAVEWORN_SHARD: 'waveworn_shard',
 });
