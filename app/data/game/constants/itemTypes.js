eb4717137e0812e5a2c60b555369665509ba7b1b
ShadesByte <225610230+ShadesByte@users.noreply.github.com>
2025-12-30 23:03:11 -0600

    Add item type constants for game resources

---
+++ app/data/game/constants/itemTypes.js
@@ -0,0 +1,9 @@
+export const ITEM_TYPE_VERSION = '3.0'
+
+export const ITEM_TYPES = Object.freeze({
+    FORGERY: 'tiered_forgery_weapon_skill_material',
+    ENEMY_DROP: 'tiered_enemy_drop_weapon_skill_material',
+    ASCENSION: 'boss_ascension_material',
+    WEEKLY_BOSS: 'weekly_boss_skill_upgrade_material',
+    OVERWORLD: 'overworld_resource_ascension_material',
+});
