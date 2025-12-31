7070da5bd5b879a7ebc916bc61204aa119f2e926
ShadesByte <225610230+ShadesByte@users.noreply.github.com>
2025-12-30 22:42:49 -0600

    Add weapon types constants

---
+++ app/data/game/constants/weaponTypes.js
@@ -0,0 +1,9 @@
+export const WEAPON_TYPE_VERSION = '3.0'
+
+export const WEAPON_TYPES = objectEntries.freeze ({
+    BROADBLADE: 'broadblade',
+    GAUNTLETS: 'gauntlets',
+    PISTOLS: 'pistols',
+    RECTIFIER: 'rectifier',
+    SWORD: 'sword',
+});
