8bc4bf5115cd5ab99a5dd09da4bed8aaf46ec9a7
ShadesByte <225610230+ShadesByte@users.noreply.github.com>
2026-01-01 21:56:12 -0600

    Refactor experience material definitions to use constants

--- app/data/game/inventoryItem/gameInventoryItemConfig.js
+++ app/data/game/inventoryItem/gameInventoryItemConfig.js
@@ -2,34 +2,37 @@
   This is for configured data, such as tiered materials
 */
 
+import { MATERIAL_CATEGORIES } from '@/data/game/constants/materialCategories';
+import { ITEM_IDS } from '@/data/game/constants/itemIDs';
+
 export const resonator_exp_material = {
 	// resonance_potion
-	basic_resonance_potion: {
+	[ITEM_IDS.BASIC_RESONANCE_POTION]: {
 		exp_value: 1000,
 	},
-	medium_resonance_potion: {
+	[ITEM_IDS.MEDIUM_RESONANCE_POTION]: {
 		exp_value: 3000,
 	},
-	advanced_resonance_potion: {
+	[ITEM_IDS.ADVANCED_RESONANCE_POTION]: {
 		exp_value: 8000,
 	},
-	premium_resonance_potion: {
+	[ITEM_IDS.PREMIUM_RESONANCE_POTION]: {
 		exp_value: 20000,
 	},
 };
 
 export const weapon_exp_material = {
 	// energy_core
-	basic_energy_core: {
+	[ITEM_IDS.BASIC_ENERGY_CORE]: {
 		exp_value: 1000,
 	},
-	medium_energy_core: {
+	[ITEM_IDS.MEDIUM_ENERGY_CORE]: {
 		exp_value: 3000,
 	},
-	advanced_energy_core: {
+	[ITEM_IDS.ADVANCED_ENERGY_CORE]: {
 		exp_value: 8000,
 	},
-	premium_energy_core: {
+	[ITEM_IDS.PREMIUM_ENERGY_CORE]: {
 		exp_value: 20000,
 	},
 };
@@ -62,10 +65,9 @@ export const tiered_materials_all = {
 };
 
 export const tiered_materials_per_type = {
-	tiered_enemy_drop_weapon_skill_material:
+	[MATERIAL_CATEGORIES.ENEMY_DROP]:
 		tiered_enemy_drop_weapon_skill_material_index_category,
-	tiered_forgery_weapon_skill_material:
-		tiered_forgery_weapon_skill_material_index_category,
+	[MATERIAL_CATEGORIES.FORGERY]: tiered_forgery_weapon_skill_material_index_category,
 	tiered_char_exp: tiered_char_exp_index_category,
 	tiered_weap_exp: tiered_weap_exp_index_category,
 };
